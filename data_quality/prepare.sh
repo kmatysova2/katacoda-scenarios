459365ac1dc5a14cd8bbdf6bc91f7bfc426b2d74
kmatysova <kmatysova2@gmail.com>
2021-11-13 10:17:02 +0100

    ...

--- data_quality/prepare.sh
+++ data_quality/prepare.sh
@@ -1,10 +1,12 @@
-docker run -p 8888:8888 -d --name jupyter jupyter/scipy-notebook:83ed2c63671f
+git clone https://github.com/kmatysova2/katacoda-scenarios.git
 
-# python
+cd katacoda-scenarios/data_quality
 
-# import pandas as pd
-# import re
-# import datetime
+python
 
-#customer_data = pd.read_csv('https://github.com/kmatysova2/katacoda-scenarios/blob/main/data_quality/data/customer_table.csv')
-#order_data = pd.read_csv('https://github.com/kmatysova2/katacoda-scenarios/blob/main/data_quality/data/order_table.csv')
+import pandas as pd
+import re
+import datetime
+
+customer_data = pd.read_csv('/data/customer_table.csv')
+order_data = pd.read_csv('/data/order_table.csv')
