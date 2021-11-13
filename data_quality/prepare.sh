33b72cc0262ab1be7331bb79b639dba02db821d5
kmatysova <kmatysova2@gmail.com>
2021-11-13 09:52:23 +0100

    ...

--- data_quality/prepare.sh
+++ data_quality/prepare.sh
@@ -4,5 +4,5 @@ import pandas as pd
 import re
 import datetime
 
-customer_data = pd.read_csv('data/customer_table.csv')
-order_data = pd.read_csv('data/order_table.csv')
+customer_data = pd.read_csv('/data/customer_table.csv')
+order_data = pd.read_csv('/data/order_table.csv')
